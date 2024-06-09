5a9e9df44e3b588d2e74b430de1be351e16f69ae
irina <obesyuk@gmail.com>
2024-06-09 19:12:57 +0300

    junit4 created

---
+++ src/test/java/CashbackServiceJunit4Test.java
@@ -0,0 +1,39 @@
+import org.example.CashbackHackService;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackServiceJunit4Test {
+
+    @Test
+    public void shouldTestTheRemainingAMount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestIfAmountIsEqualToBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestIfAmountIs500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
